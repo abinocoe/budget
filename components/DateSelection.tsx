642b2778504baf70f98a0078743c937662978e1a
Abigail Coe <abigailcoe@gmail.com>
2019-02-10 14:34:03 +0000

    show/hide date picker

--- components/DateSelection.tsx
+++ components/DateSelection.tsx
@@ -1,6 +1,6 @@
 import _ from "lodash";
 import React, { Component } from "react";
-import { Picker } from "react-native";
+import { Picker, Text, TouchableOpacity, View } from "react-native";
 
 const DAYSOFMONTH = _.range(1, 32);
 
@@ -9,19 +9,40 @@ interface Props {
   intervalStartDate: number;
 }
 
-class DateSelection extends Component<Props> {
+interface State {
+  showPicker: boolean;
+}
+
+class DateSelection extends Component<Props, State> {
+  public state: Readonly<State> = {
+    showPicker: false
+  };
+
   public render() {
-    return (
-      <Picker
-        selectedValue={this.props.intervalStartDate}
-        onValueChange={itemValue =>
-          this.props.updateIntervalStartDate(itemValue)
-        }
+    return this.state.showPicker ? (
+      <View>
+        <TouchableOpacity
+          onPress={() => this.setState({ showPicker: !this.state.showPicker })}
+        >
+          <Text>X</Text>
+        </TouchableOpacity>
+        <Picker
+          selectedValue={this.props.intervalStartDate}
+          onValueChange={itemValue =>
+            this.props.updateIntervalStartDate(itemValue)
+          }
+        >
+          {DAYSOFMONTH.map(date => (
+            <Picker.Item label={date.toString()} value={date} key={date} />
+          ))}
+        </Picker>
+      </View>
+    ) : (
+      <TouchableOpacity
+        onPress={() => this.setState({ showPicker: !this.state.showPicker })}
       >
-        {DAYSOFMONTH.map(date => (
-          <Picker.Item label={date.toString()} value={date} key={date} />
-        ))}
-      </Picker>
+        <Text>{this.props.intervalStartDate}</Text>
+      </TouchableOpacity>
     );
   }
 }
