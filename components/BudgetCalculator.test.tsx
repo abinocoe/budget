3175e813fe47aaadaf2618cd73e49fb2cbd84fd8
Abigail Coe <abigailcoe@gmail.com>
2019-02-25 11:02:35 +0000

    mock date in tests

--- components/BudgetCalculator.test.tsx
+++ components/BudgetCalculator.test.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import BudgetCalculator from "./BudgetCalculator";
 
 it("renders correctly", () => {
+  Date.now = jest.fn(() => 1487076708000);
   const output = shallow(<BudgetCalculator />);
   expect(output).toMatchSnapshot();
 });
