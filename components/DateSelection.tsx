c3b930a4c38d6bfa5afa524cc2ca3730c2db347f
Abigail Coe <abigailcoe@gmail.com>
2019-03-27 17:26:01 +0000

    much prettier date selection

--- components/DateSelection.tsx
+++ components/DateSelection.tsx
@@ -1,10 +1,14 @@
 import _ from "lodash";
 import React, { Component } from "react";
-import { Picker, Text, TouchableOpacity, View } from "react-native";
+import { Text, View } from "react-native";
+import { CustomPicker } from "react-native-custom-picker";
 
 import * as colours from "../constants/colours";
 
 const DAYSOFMONTH = _.range(1, 32);
+const DaysObjects = DAYSOFMONTH.map(date => {
+  return { value: date, label: date.toString() };
+});
 
 interface Props {
   updateIntervalStartDate: (intervalStart: number) => void;
@@ -23,41 +27,59 @@ class DateSelection extends Component<Props, State> {
   public render() {
     return (
       <>
-        {this.state.showPicker ? (
-          <Picker
-            selectedValue={this.props.intervalStartDate}
-            onValueChange={itemValue => {
-              this.props.updateIntervalStartDate(itemValue);
-              this.setState({ showPicker: false });
-            }}
-          >
-            {DAYSOFMONTH.map(date => (
-              <Picker.Item label={date.toString()} value={date} key={date} />
-            ))}
-          </Picker>
-        ) : (
-          <TouchableOpacity onPress={() => this.setState({ showPicker: true })}>
-            <View style={{ alignItems: "center" }}>
-              <Text
-                style={{
-                  fontSize: 30,
-                  paddingHorizontal: 5,
-                  marginTop: 10,
-                  marginBottom: 5,
-                  borderRadius: 5,
-                  width: "90%",
-                  backgroundColor: colours.blue,
-                  textAlign: "center",
-                  color: "white",
-                  elevation: 3
-                }}
-              >
-                {this.props.intervalStartDate}
-              </Text>
-              <Text style={{ fontSize: 12 }}>Period start date</Text>
-            </View>
-          </TouchableOpacity>
-        )}
+        <CustomPicker
+          options={DaysObjects}
+          onValueChange={itemValue => {
+            this.props.updateIntervalStartDate(itemValue.value);
+            this.setState({ showPicker: false });
+          }}
+          fieldTemplate={() => {
+            return (
+              <View style={{ alignItems: "center" }}>
+                <Text
+                  style={{
+                    fontSize: 30,
+                    paddingHorizontal: 5,
+                    marginTop: 10,
+                    marginBottom: 5,
+                    borderRadius: 5,
+                    width: "90%",
+                    backgroundColor: colours.blue,
+                    textAlign: "center",
+                    color: "white",
+                    elevation: 3
+                  }}
+                >
+                  {this.props.intervalStartDate}
+                </Text>
+                <Text style={{ fontSize: 12 }}>Period start date</Text>
+              </View>
+            );
+          }}
+          optionTemplate={settings => {
+            const { item } = settings;
+            return (
+              <View style={{ alignItems: "center" }}>
+                <Text
+                  style={{
+                    fontSize: 30,
+                    paddingHorizontal: 5,
+                    marginTop: 10,
+                    marginBottom: 5,
+                    borderRadius: 5,
+                    width: "40%",
+                    backgroundColor: colours.blue,
+                    textAlign: "center",
+                    color: "white",
+                    elevation: 3
+                  }}
+                >
+                  {item.label}
+                </Text>
+              </View>
+            );
+          }}
+        />
       </>
     );
   }
