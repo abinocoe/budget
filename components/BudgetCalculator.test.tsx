22b99dbdb4ecb2b2d22c2a48fa3586664b981b48
Abigail Coe <abigailcoe@gmail.com>
2019-03-29 17:20:47 +0000

    sort out some AsyncStorage mocking

--- components/BudgetCalculator.test.tsx
+++ components/BudgetCalculator.test.tsx
@@ -1,5 +1,6 @@
 import { shallow } from "enzyme";
 import React from "react";
+import { AsyncStorage } from "react-native";
 
 // @ts-ignore
 import { dateToday, today } from "../lib/date";
@@ -7,6 +8,15 @@ import { dateToday, today } from "../lib/date";
 import BudgetCalculator from "./BudgetCalculator";
 
 const currentDate = new Date();
+const nextTick = () => new Promise(resolve => setTimeout(resolve, 0));
+
+beforeAll(() => {
+  jest.mock("AsyncStorage");
+});
+
+beforeEach(() => {
+  jest.clearAllMocks();
+});
 
 afterEach(() => {
   today.setDate(currentDate.getDate());
@@ -16,12 +26,20 @@ afterEach(() => {
   dateToday = today.getDate();
 });
 
-it("renders correctly", () => {
+it("renders correctly", async () => {
   today.setDate(25);
   today.setMonth(2);
   today.setFullYear(2019);
   // @ts-ignore
   dateToday = 25;
+  await nextTick();
   const output = shallow(<BudgetCalculator />);
   expect(output).toMatchSnapshot();
+});
+
+it("", async () => {
+  shallow(<BudgetCalculator />);
+  await nextTick();
+  expect(AsyncStorage.multiGet).toHaveBeenCalledTimes(1);
+  expect(AsyncStorage.getItem).toHaveBeenCalledTimes(2);
 });
